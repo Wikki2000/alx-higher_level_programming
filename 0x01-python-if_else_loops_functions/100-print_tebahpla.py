ec7a7a5d1666cb64fbc2bdfca8370ed7ef78b060
wikki2000 <wisdomokposin@gmail.com>
2023-11-29 02:06:09 -0800

    Add files

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 
 for c in range(ord('z'), ord('A') - 1, -1):
     if 'a' <= chr(c) <= 'z' or 'A' <= chr(c) <= 'Z':
-        print(f"{chr(c)}" if (ord('z') - c) % 2 == 0 else f"{chr(c - 32)}", end="")
+        print("{}".format(chr(c)) if (ord('z') - c + 1) % 2 == 0 else f"{chr(c - 32)}", end="")
