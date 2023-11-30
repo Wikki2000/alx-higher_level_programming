0dfbdf1665afeff4dc1c8accc859482b73a330f6
wikki2000 <wisdomokposin@gmail.com>
2023-11-30 08:16:18 -0800

    Add files 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,9 @@
 from sys import argv
 
 if __name__ == "__main__":
-    argc = len(argv) - 1
-    if argc == 0:
+    argc = len(argv)
+    if argc == 1:
         print("0 arguments.")
     else:
-        print("{} argument:".format(argc))
-        for i in range(1, argc + 1):
+        for i in range(1, argc):
             print("{}: {}".format(i, argv[i]))
