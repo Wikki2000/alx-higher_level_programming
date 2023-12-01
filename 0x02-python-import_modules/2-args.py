86e4ad79216f33dc69e24a877cdeba9d50889867
wikki2000 <wisdomokposin@gmail.com>
2023-12-01 01:59:45 -0800

    Resolve error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     argc = len(argv)
     if argc == 1:
         print("0 arguments.")
-    else:
+    elif argc == 2:
         print("{} argument:".format(argc - 1))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
             print("{}: {}".format(i, argv[i]))
