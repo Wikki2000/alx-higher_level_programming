771b373435c846c5859eb6754f8be281d59c7c36
wikki2000 <wisdomokposin@gmail.com>
2023-12-03 06:37:46 -0800

    Resolve coding style conflict

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,6 +6,6 @@ def max_integer(my_list=[]):
     else:
         max_num = my_list[0]
         for num in my_list:
-            if num > max_num: 
+            if num > max_num:
                 max_num = num
         return (max_num)
