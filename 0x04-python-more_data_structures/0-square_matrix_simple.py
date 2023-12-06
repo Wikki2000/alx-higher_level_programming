dfd6cfb2060d821fbc0c0b795e452389d678b64d
wikki2000 <wisdomokposin@gmail.com>
2023-12-06 07:10:13 -0800

    Add files

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+def sq(x):
+    return x ** 2
+
+
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
-    sq = lambda x: x ** 2
     new_matrix = [list(map(sq, row)) for row in new_matrix]
     return new_matrix
+
+
+"""
+Another method to use is:
+def square_matrix_simple(matrix=[]):
+    return [[x**2 for x in row] for row in matrix]
+"""
