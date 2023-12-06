1347c4c689e290c466f40a080e6ddcd7fab32101
wikki2000 <wisdomokposin@gmail.com>
2023-12-06 09:31:43 -0800

    Resolve error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if not my_list:
+        return []
     new_list = my_list.copy()
     for i in range(len(my_list)):
         if new_list[i] == search:
             new_list[i] = replace
     return new_list
 
+"""
+Alternatives method:
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list == [] or my_list is None:
+        return []
+    new = [replace if item is search else item for item in my_list]
+    return new
+"""
