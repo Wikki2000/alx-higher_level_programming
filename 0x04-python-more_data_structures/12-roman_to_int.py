bf0119793fe92b23df3701e31e50d68cfdcac182
wikki2000 <wisdomokposin@gmail.com>
2023-12-10 05:29:43 -0800

    Add file 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) != str or not roman_string:
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 100}
+    prev_value = 0
+    total = 0
+    for numeral in roman_string:
+        current_value = roman_dict[numeral]
+        if current_value >= prev_value:
+            total += current_value - 2 * prev_value
+        else:
+            total += value
+    return total
