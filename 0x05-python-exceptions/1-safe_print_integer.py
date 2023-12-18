e885d4529063f37092a232f0d774572d8038694a
wikki2000 <wisdomokposin@gmail.com>
2023-12-18 09:01:41 -0800

    Add files

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if isinstance(value, int) == 1:
-            print("{:d}".format(value))
+        print("{:d}".format(value))
         return True
-    except TypeError:
+    except ValueError:
         return False
