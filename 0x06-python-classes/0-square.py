6e95b9ec5c5a8127c421507215ec0b4a818c13be
wikki2000 <wisdomokposin@gmail.com>
2023-12-19 07:06:25 -0800

    Add files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""Square module: this class <Square> models a Square object"""
+
+
 class Square:
-    """Empty class definition for a square."""
+    """Squre class
+
+    this class is a minimal square class with no implementation
+    just pass.
+
+    """
     pass
