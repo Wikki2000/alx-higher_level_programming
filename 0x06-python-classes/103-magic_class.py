3580bcc24fe090a48e0deb1489813c687ab6751e
wikki2000 <wisdomokposin@gmail.com>
2023-12-20 01:44:08 -0800

    Addd files

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import math
+
+class MagicClass:
+    """MagicClass with similar functionality as the provided bytecode."""
+
+    def __init__(self, radius=0):
+        """Constructor for MagicClass.
+
+        Args:
+            radius (int or float): Radius of the circle. Defaults to 0.
+
+        Raises:
+            TypeError: If radius is not a number.
+        """
+        if type(radius) not in [int, float]:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """Calculate and return the area of the circle."""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """Calculate and return the circumference of the circle."""
+        return 2 * math.pi * self.__radius
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!usr/bin/python3
+
+"""Square module: This <square> module models a sqaure class"""
+
+
+class Square:
+    """Class definition of a square"""
+
+    def __init__(self, size=0):
+        """__init__ method
+        This create an instance of a class
+
+        Args:
+            size (int): The <size> attribute of a square
+        """
+        self.__size = size
+
+    @property
+    def size(sef):
+        """Getter method of <size> attribute"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method of size attribute
+
+        Args:
+            Value (int): The size of the square class
+
+        Raises:
+            TypeError: If size not an integer
+            ValueError: If size less than 0
+        """
+        self.__size = value
+
+    def area(self):
+        """Area method: Calculate area of a square
+
+        Return:
+            int: Return the square of the size
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """my_print method: Print the square using '#' character"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end=" ")
+                print()
