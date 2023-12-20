1d9034f8334216fbb166e55a84cb015c820631ae
wikki2000 <wisdomokposin@gmail.com>
2023-12-20 01:51:39 -0800

    Resolve bug

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,6 +31,10 @@ class Square:
             TypeError: If size not an integer
             ValueError: If size less than 0
         """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
