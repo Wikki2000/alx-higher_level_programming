951c9f86357d894d1e6fa7fd9cfbf50ceaeb4a1b
wikki2000 <wisdomokposin@gmail.com>
2024-01-06 02:32:13 -0800

    Update repo

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Square module: This class model a rectangle"""
+
+
+class Rectangle:
+    """Class definition of a Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Task on 0x08-python-more_classes
