82c7b0747b5fa1ccb09002f70efc99e5a0b9d307
wikki2000 <wisdomokposin@gmail.com>
2024-01-06 04:19:25 -0800

    Add file

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,38 +1,62 @@
 #!/usr/bin/python3
-"""create a class"""
+
+"""Rectangle module: This class models a Rectangle"""
 
 
 class Rectangle:
-    """instantiate attributes"""
+    """This class defines a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """__init__ method
+        This method creates an instance of the class
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+        """
         self.width = width
         self.height = height
 
-    """create a property method for private instance"""
     @property
     def width(self):
+        """Getter method for <width> attribute"""
         return self.__width
 
-    """retrieve the property instance"""
     @width.setter
     def width(self, value):
-        """check if width is an integer"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+        """Setter method for <width> attribute
+
+        Args:
+            value (int): The width of the rectangle to be set
+
+        Raises:
+            TypeError: If width is not an integer
+            ValueError: If width is less than 0
+        """
         if value < 0:
             raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
         self.__width = value
 
     @property
     def height(self):
+        """Getter method for <height> attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """check if height is an integer"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+        """Setter method for <height> attribute
+
+        Args:
+            value (int): The height of the rectangle to be set
+
+        Raises:
+            TypeError: If height is not an integer
+            ValueError: If height is less than 0
+        """
         if value < 0:
             raise ValueError("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
         self.__height = value
