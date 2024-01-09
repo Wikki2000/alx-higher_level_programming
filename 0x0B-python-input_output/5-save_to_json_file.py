bea3b75910e68a88c68ffe44ce913f75ca228348
wikki2000 <wisdomokposin@gmail.com>
2024-01-09 06:58:28 -0800

    Add files

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -11,5 +11,6 @@ from json import dump
 
 
 def save_to_json_file(my_obj, filename):
+    """Write json string to a file"""
     with open(filename, 'w', encoding='UTF-8') as file:
         dump(my_obj, file)
