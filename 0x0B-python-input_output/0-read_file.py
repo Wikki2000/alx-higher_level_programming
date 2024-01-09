90647c32e6825bb489bdd00995ee41d461c1880d
wikki2000 <wisdomokposin@gmail.com>
2024-01-09 05:23:50 -0800

    Add files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
+"""
+==================
+module 0-read_file
+==================
+"""
+
+
 def read_file(filename=""):
+    """Function to read file"""
+
     with open(filename, 'r', encoding='UTF-8') as file:
         content = file.read()
-        print(content)
+        print(content, end="")
