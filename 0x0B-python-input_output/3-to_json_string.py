17e5b8acd3bd92fff76eba1d0d6a622ef85a08c8
wikki2000 <wisdomokposin@gmail.com>
2024-01-09 06:20:40 -0800

    Add files

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -9,7 +9,7 @@
 
 from json import dumps
 
-
 def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object (string)"""
     json_string = dumps(my_obj)
     return json_string
