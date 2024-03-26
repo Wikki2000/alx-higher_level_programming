cdb951afb895ead5c1a97c29a688048dce0d2cfa
wikki2000 <wisdomOkposin@gmail.com>
2024-03-26 14:35:15 -0700

    Add files

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,9 +1,3 @@
--- CreateDatabase.sql
-
--- Check if the database exists
-USE hbtn_0c_0;
-IF @@ROWCOUNT = 0 THEN
-    -- Create the database
-    CREATE DATABASE hbtn_0c_0;
-END IF;
-
+-- Creates the database hbtn_0c_0 in MySQL server
+-- If it does not exist
+CREATE hbtn_0c_0 IF NOT EXISTS
