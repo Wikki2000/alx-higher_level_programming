8f0cb45275c23a1f9737a9bbb5a34435da286416
wikki2000 <wisdomOkposin@gmail.com>
2024-03-27 06:31:06 -0700

    Add files

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
 -- Display all records with a score >= 10
 -- in the table second_table of the database hbtn_0c_0
-SELECT name, score
+SELECT score, name
 FROM second_table
 WHERE SCORE >= 10
 ORDER BY score DESC
