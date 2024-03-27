15d0f844fc1edb628546cb144d6a255f70e70c59
wikki2000 <wisdomOkposin@gmail.com>
2024-03-27 06:45:12 -0700

    Add files

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0c_0 in your MySQL server.
 -- update score using name col
-SELECT AVG(score) as average
-FROM second_table;
+SELECT AVG(score) AS avg_score
+FROM second_table
