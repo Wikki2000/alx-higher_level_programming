9db8aee1faa5b0a55e058354cb664c1783ea786c
wikki2000 <wisdomOkposin@gmail.com>
2024-03-28 07:07:02 -0700

    Add files

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
---- Ccreates the MySQL server user user_0d_1 with password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+-- Create the MySQL server user user_0d_1 with password
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1 on your MySQL server
-GRANT ALL PRIVILEGES ON *.* TO 'newuser'@'localhost'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
