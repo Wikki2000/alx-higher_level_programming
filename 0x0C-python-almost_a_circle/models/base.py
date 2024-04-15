78b9ef2cf12f7cff81caa73027684528d42710cf
wikki2000 <wisdomokposin@gmail.com>
2024-04-15 16:28:09 -0700

    Add files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 """Module for super class"""
+from json import dumps
 
 
 class Base:
@@ -18,3 +18,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
