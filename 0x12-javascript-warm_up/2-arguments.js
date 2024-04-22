e4d4065a1de2b4211d96ac721c2e21df0a703ec9
wikki2000 <wisdomokposin@gmail.com>
2024-04-22 10:24:37 -0700

    Add files

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-let args = process.argv
-if (args.length === 2)
-{
-	console.log('No argument');
+let args = process.argv;
+if (args.length === 2) {
+    console.log('No argument');
+} else {
+    console.log('Argument found');
 }
-else
-{
-	console.log('Argument found');
-}
+
