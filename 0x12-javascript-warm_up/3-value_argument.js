877a2c91ae750ef2cac3b98e5c055e067d7035ea
wikki2000 <wisdomokposin@gmail.com>
2024-04-22 10:40:19 -0700

    Add files

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 
 let args = process.argv
-if (args[2] !== undefined)
-{
+if (args[2] !== undefined) {
 	console.log(args[2]);
-}
-else
-{
-	console.log('No argument')
-}
+} else {
+	console.log('No argument')}
