406d31139b3c3651852249949a5e9e4347177076
wikki2000 <wisdomokposin@gmail.com>
2024-04-23 10:05:16 -0700

    Add files

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor(size) { 
+    super(size, size); // call constructor of parent class
+  }
+
+  charPrint(c) {
+    if (c == undefined) {
+      this.print();
+    } else { 
+      for (let i = 0; i < this.height; i++) { 
+        for (let j = 0; j < this.width; j++) { 
+          process.stdout.write(c);
+        }
+        process.stdout.write('\n');
+      }
+    }
+  } 
+} 
+
+module.exports = Square;
