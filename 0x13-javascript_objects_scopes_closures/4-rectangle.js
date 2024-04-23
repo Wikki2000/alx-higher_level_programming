04a2aab3c5d087eb7b37e8b9110fa439328aa2de
wikki2000 <wisdomokposin@gmail.com>
2024-04-23 10:39:13 -0700

    Add files

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if ((w > 0 && !isNaN(w)) && ( h > 0 && !isNaN(h))) {
       this.width = w;
       this.height = h;
     }
