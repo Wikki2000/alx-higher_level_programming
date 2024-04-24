82b9ad67292e2241226ad789d40987f8cb79f653
wikki2000 <wisdomokposin@gmail.com>
2024-04-24 09:50:52 -0700

    Add files

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
 exports.add = function (a, b) {
   const sum = Number(a) + Number(b);
-#!/usr/bin/node
-function add (a, b) {
-  const sum = Number(a) + Number(b);
-  return sum;
-}
-exports.add = function;
   return sum;
 };
