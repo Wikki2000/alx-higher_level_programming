9961d2a7531174993b5f2c37b7097eaf2109eb74
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-04-26 13:33:27 +0100

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,53 @@
 #!/usr/bin/python3
-
 """
-    module to list all state object from the database
+Script to list all State objects from the database hbtn_0e_6_usa
 """
 
-from model_state import Base, State
 import sys
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-if __name__ == '__main__':
-    # connect to the database
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    # create a session with the connected database
-    Session = sessionmaker(bind=engine)
+def list_states(username, password, database):
+    """
+    Function to list all State objects from the database hbtn_0e_6_usa
+
+    Args:
+        username (str): MySQL username
+        password (str): MySQL password
+        database (str): MySQL database name
+
+    Returns:
+        None
+    """
+    # Construct the database URL
+    db_url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database)
+
+    # Create engine and establish connection
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine) # bind and create class of session
     session = Session()
 
-    # using the session, communicate with the database
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print('Nothing')
-    else:
-        print("{}: {}".format(state.id, state.name))
+    try:
+        # Retrieve and print all State objects sorted by id
+        states = session.query(State).order_by(State.id).first()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+    except Exception as e:
+        print("Error:", e)
+    finally:
+        # Close the session
+        session.close()
+
+if __name__ == "__main__":
+    # Check if correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract MySQL username, password, and database name
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Call the function to list states
+    list_states(username, password, database)
