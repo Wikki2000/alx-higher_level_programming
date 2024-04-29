c3697a8e0fbdb2270c50bcae6368906ca9b22ada
wikki2000 <wisdomokposin@gmail.com>
2024-04-29 12:31:20 -0700

    Add files

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,6 @@ Models the city table in database
 """
 from sqlalchemy import String, Integer, Column, ForeignKey
 from model_state import Base
-from sqlalchemy.orm import relationship
 
 
 class City(Base):
@@ -17,6 +16,7 @@ class City(Base):
         state_id (int): Foreign key linking to state table
     """
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, autoincrement=True, nullable=False, primary_key=True)
+    id = Column(Integer, unique=True, autoincrement=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
