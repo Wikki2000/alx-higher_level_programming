b5c791ba21670abdf6e17441f1af61426629780f
wikki2000 <wisdomokposin@gmail.com>
2024-04-29 07:39:19 -0700

    Add files

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     # Create a session with the connected database
     Session = sessionmaker(bind=engine)
     session = Session()
