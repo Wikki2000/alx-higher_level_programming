87c9d84f9adf39c21f6a8ac092736ef2a60aa9b4
wikki2000 <wisdomokposin@gmail.com>
2024-07-04 20:24:55 +0100

    Update repo

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response.
-
-# Check if URL is provided
-if [ -z "$1" ]; then
-	echo "Usage: $0 <url>"
-else
-	# Send request to URL and display size of the body in bytes
-		response_size=$(curl -s "$1" | wc -c)
-	echo "$response_size"
-fi
+curl -s "$1" | wc -c
