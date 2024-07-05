af6393dc59c99efc328ad363938b36bd22b576cf
Wisdom Okposin <140092860+Wikki2000@users.noreply.github.com>
2024-07-06 00:39:04 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Fetches https://intranet.hbtn.io/status with a User-Agent."""
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    url = "https://intranet.hbtn.io/status"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    request = urllib.request.Request(url, headers=headers)
+    try:
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    except urllib.error.HTTPError as e:
+        print(f'HTTPError: {e.code} {e.reason}')
+    except urllib.error.URLError as e:
+        print(f'URLError: {e.reason}')
