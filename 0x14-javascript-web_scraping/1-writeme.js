317dfe7360233a0a29354b762e35b25535d0cea9
wikki2000 <wisdomokposin@gmail.com>
2024-07-30 23:05:06 +0100

    add: 0-readme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const args = process.argv;
 
 try {
-  console.log(fs.readFileSync(args[2], 'utf8'));
+  fs.writeFileSync(args[2], args[3], 'utf8');
 } catch (err) {
   console.log(err);
 }
