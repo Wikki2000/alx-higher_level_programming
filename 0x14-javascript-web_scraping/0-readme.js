d7606ae846be73f3f9a46f9f99e71fb41830c279
wikki2000 <wisdomokposin@gmail.com>
2024-07-30 22:55:20 +0100

    add: files

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
-const fs = require("fs");
 
+const fs = require("fs");
 const args = process.argv;
+const filePath = args[2];
 
 if (args.length !== 3) {
     console.log(`Usage: ${args[0]} ${args[1]} <file_path>`);
     process.exit(1);
 }
-
-const filePath = args[2];
 
 try {
     const data = fs.readFileSync(filePath, "utf-8");
