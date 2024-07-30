00cd3f3d58ad41bbf8722017debd6b70e0995867
wikki2000 <wisdomokposin@gmail.com>
2024-07-30 22:59:30 +0100

    add: 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,17 +1,10 @@
 #!/usr/bin/node
 
-const fs = require("fs");
+const fs = require('fs');
 const args = process.argv;
-const filePath = args[2];
-
-if (args.length !== 3) {
-    console.log(`Usage: ${args[0]} ${args[1]} <file_path>`);
-    process.exit(1);
-}
 
 try {
-    const data = fs.readFileSync(filePath, "utf-8");
-    console.log(data);
+  console.log(fs.readFileSync(args[2], 'utf8'));
 } catch (err) {
-    console.error(err);
+  console.log(err);
 }
