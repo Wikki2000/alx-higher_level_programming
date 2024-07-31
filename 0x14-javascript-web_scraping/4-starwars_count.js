4c753edade3c76435bd80c4ca8388fc1376d2553
wikki2000 <wisdomokposin@gmail.com>
2024-07-31 13:32:31 +0100

    add two files

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/';
+const args = process.argv;
+const url = args[2];
 
 request.get(url, (error, response, body) => {
     if (error) return console.log(error);
