68cc5d6065dd92967e4a49cb27cc432368cffb73
wikki2000 <wisdomokposin@gmail.com>
2024-07-31 13:36:16 +0100

    update:  4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,13 +5,13 @@ const args = process.argv;
 const url = args[2];
 
 request.get(url, (error, response, body) => {
-    if (error) return console.log(error);
+  if (error) return console.log(error);
 
-    let count = 0;
-    JSON.parse(body).results.forEach(movie => {
-        movie.characters.forEach(character => {
-            if (character.includes('18')) count++;
-        });
+  let count = 0;
+  JSON.parse(body).results.forEach(movie => {
+    movie.characters.forEach(character => {
+      if (character.includes('18')) count++;
     });
-    console.log(count);
+  });
+  console.log(count);
 });
